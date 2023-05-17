01f23a84aec04f2715231e94674be6f78e2ac139
jankubin <101505317+jankubin@users.noreply.github.com>
2023-05-17 15:34:37 +0200

    Initial Commit

---
+++ VypocetCestaku/VypocetCestakuApp.swift
@@ -0,0 +1,17 @@
+//
+//  VypocetCestakuApp.swift
+//  VypocetCestaku
+//
+//  Created by Jan Kubín on 17.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct VypocetCestakuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
